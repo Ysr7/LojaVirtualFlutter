426f88237dd6065bb3e3c07bdef25c313ba47bfe
Yuri Range <ysr7santos@gmail.com>
2020-09-28 19:12:26 -0300

    Buscando os produtos no fire base

--- loja_virtual/lib/main.dart
+++ loja_virtual/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/product_manager.dart';
 import 'package:loja_virtual/models/user_manager.dart';
 import 'package:loja_virtual/screens/base/base_sreeen.dart';
 import 'package:loja_virtual/screens/login/login_screen.dart';
@@ -11,9 +12,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => UserManager(),
-      lazy: false,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UserManager(),
+          lazy: false,
+        ),
+        Provider(
+          create: (_) => ProductManager(),
+          lazy: false,
+        )
+      ],
       child: MaterialApp(
         title: 'Loja Virtual',
         debugShowCheckedModeBanner: false,
---
+++ loja_virtual/lib/models/product_manager.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ProductManager {
+  
+  ProductManager() {
+    _loadAllProducts();
+  }
+
+  final Firestore firestore = Firestore.instance;
+
+  Future<void> _loadAllProducts() async {
+    QuerySnapshot snapProducts =
+        await firestore.collection("products").getDocuments();
+
+  }
+}
